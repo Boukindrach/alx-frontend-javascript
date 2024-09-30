7318c922e982106c2567a908f81dfa226c0825cb
root <Rachid Boukind>
2024-09-30 18:27:41 +0000

    Block scope task

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,7 +3,8 @@ export default function taskBlock(trueOrFalse) {
   var task2 = true;
 
   if (trueOrFalse) {
-    return [task, task2];
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
