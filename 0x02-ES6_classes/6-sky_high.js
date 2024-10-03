99a8430aeb8379cdd30e42b6528b9bdabfa3dcfe
Boukind Rachid <boukind.r1@gmail.com>
2024-10-03 19:50:52 +0000

    fix some errors

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,27 +1,18 @@
 import Building from './5-building';
 
-export default class SkyHighBuilding extends Building {
+class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this.floors = floors;
-  }
-
-  get sqft() {
-    return this._sqft;
+    this._floors = floors;
   }
 
   get floors() {
-    return this._floors;
-  }
-
-  set floors(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('floors must be a number');
-    }
-    this._floors = value;
+    return (this._floors);
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this.floors} floors`;
+    return (`Evacuate slowly the ${this.floors} floors`);
   }
 }
+
+export default SkyHighBuilding;
