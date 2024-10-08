46473f298ca84a9692fa5d7221062e3db643f36f
Boukind Rachid <boukind.r1@gmail.com>
2024-10-08 03:35:20 +0000

    style fixing

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
 export default function getStudentIdsSum(students) {
-    return students.reduce((accpet, c) => accpet + c.id, 0);
-  }
+  return students.reduce((accpet, c) => accpet + c.id, 0);
+}
