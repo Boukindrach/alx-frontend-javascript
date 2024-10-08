e4c5857cb59bc58243a5c327d1f435b8d48aa99a
Boukind Rachid <boukind.r1@gmail.com>
2024-10-08 03:49:47 +0000

    new

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -4,8 +4,8 @@ export default function cleanSet(set, startString) {
   }
 
   const filteredValues = [...set]
-    .filter(value => typeof value === 'string' && value.startsWith(startString))
-    .map(value => value.slice(startString.length));
+    .filter((value) => typeof value === 'string' && value.startsWith(startString))
+    .map((value) => value.slice(startString.length));
 
   return filteredValues.join('-');
 }
